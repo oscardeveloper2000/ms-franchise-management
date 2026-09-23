a4dea7eb7a402497175b410ed3f090c835fc8b22
Oscar Sanabria <daniloos2000@gmail.com>
2026-09-23 01:02:30 -0500

    feat(domain): add domain exception for invariant

---
+++ src/main/java/com/epam/franquicias/domain/exception/DomainValidationException.java
@@ -0,0 +1,8 @@
+package com.epam.franquicias.domain.exception;
+
+public class DomainValidationException extends RuntimeException {
+
+    public DomainValidationException(String message) {
+        super(message);
+    }
+}
