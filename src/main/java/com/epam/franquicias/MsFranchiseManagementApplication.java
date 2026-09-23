2a8f2acafa966cdc5dbc3c5f4b262d16b306215d
Oscar Sanabria <daniloos2000@gmail.com>
2026-09-23 16:53:59 -0500

    feat(application): create main application class for franchise management

---
+++ src/main/java/com/epam/franquicias/MsFranchiseManagementApplication.java
@@ -0,0 +1,12 @@
+package com.epam.franquicias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsFranchiseManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsFranchiseManagementApplication.class, args);
+    }
+}
